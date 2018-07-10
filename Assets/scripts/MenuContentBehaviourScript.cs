f3923f016605b971e1ad2544a0308b97649efc67
Shinevision <ShinevisionSV@Gmail.com>
2018-07-11 00:09:58 +0200

    Some Changes
    -UI fixes
    -Script changes

--- Assets/scripts/MenuContentBehaviourScript.cs
+++ Assets/scripts/MenuContentBehaviourScript.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 public class MenuContentBehaviourScript : MonoBehaviour {
     public GameObject ItemHolder;
@@ -10,6 +12,8 @@ public class MenuContentBehaviourScript : MonoBehaviour {
 	// Use this for initialization
 	void Start ()
     {
+
+        TestFunction();
         GameObject GO = Instantiate(ItemHolder);
         GO.transform.SetParent(transform);
         GO.transform.GetChild(0).GetComponent<Image>().sprite = sprite;
@@ -25,9 +29,33 @@ public class MenuContentBehaviourScript : MonoBehaviour {
 
         }
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    void TestFunction()
+    {
+        TestClass testClass = new TestClass(); // create random data for testing.
+        testClass.testData = "TestDataStuff";
+        testClass.nestedobject.nestedData = "nestedStuff!";
+        JObject json = new JObject(); // Creating a json object.
+        json = JsonConvert.DeserializeObject<JObject>(JsonConvert.SerializeObject(testClass)); // Converting the temp class to random json data. then deserializing it. (Simulating reading a file and getting data :P)
+        Debug.Log(json["testData"]); // Trying to read random data. if all good, you should see the data inside testData in the console in unity.
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
 		
 	}
+}
+
+public class TestClass // Temp class for testing the json stuff
+{
+    public string testData;
+    public static string testName;
+
+    public nestedObject nestedobject = new nestedObject();
+
+    public class nestedObject
+    {
+        public string nestedData;
+    }
 }
