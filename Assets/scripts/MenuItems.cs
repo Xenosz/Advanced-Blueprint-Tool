6d5078731dc6b0978f128f92ece6a68e12a79c68
Unknown <brent.swaenen@hotmail.com>
2018-07-12 22:15:00 +0200

    buttons crap

---
+++ Assets/scripts/MenuItems.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class MenuItems : MonoBehaviour
+{
+    
+
+	// Use this for initialization
+	void Start ()
+    {
+        EditorUtility.DisplayDialog("Select File", "Test2", "OK");
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public void Toggle(GameObject GO) 
+    {
+        GO.SetActive(!GO.activeSelf);
+    }
+}
